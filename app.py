63ee459123e4f44b4b889a31f6ad4f7cccdbea6b
Ajay Singh Rajput <ajay.rajput@dreamepoint.com>
2026-08-14 18:18:59 +0530

    Fix page icon filename case sensitivity

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import disease_detector
 # 1. Page Config
 st.set_page_config(
     page_title="AGRIQN - Quantum & Crop AI Doctor",
-    page_icon="logo.PNG",
+    page_icon="logo.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
